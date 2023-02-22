c8a7c927f90ae3ffa8394666a6bcbe569b218c2c
charlie42 <oninfrared@gmail.com>
2023-02-22 12:21:34 +0100

    score swan manually for adhd combined

--- score_manually.py
+++ score_manually.py
@@ -4,7 +4,6 @@
 
 import pandas as pd
 import os, datetime
-import json
 
 def get_newest_non_empty_dir_in_dir(path):
     dir_names = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
@@ -23,17 +22,17 @@ swan_cols = [col for col in all_data.columns if col.startswith("SWAN")]
 srs_cols = [col for col in all_data.columns if col.startswith("SRS")]
 diag_cols = [col for col in all_data.columns if col.startswith("Diag.")]
 
+
+
+
 ### Score SWAN manually for ADHD-Inattentive Type
+print("Diag.ADHD-Inattentive Type")
 
-data = all_data[swan_cols + diag_cols]
-print(data.columns)
-print(data) 
+data = all_data[swan_cols + diag_cols].copy()
 
-inattentive_cols = swan_cols[0:9] # First 9 cols: https://www.amerihealth.com/pdfs/providers/resources/worksheets/prevhealth_swan.pdf
-print("Inattentive cols: ", inattentive_cols)
+inattentive_cols = swan_cols[0:9] # First 9 cols: https://www.aacap.org/App_Themes/AACAP/docs/member_resources/toolbox_for_clinical_practice_and_outcomes/symptoms/SWAN_Attention-DeficitHyperactivity_Disorder_Symptoms_Child.pdf 
 
 data["Inattentive_score"] = data[inattentive_cols].sum(axis=1)/len(inattentive_cols)
-print(data[inattentive_cols + ["Inattentive_score"]])
 
 # Print mean score 
 print(data["Inattentive_score"].mean())
@@ -51,10 +50,34 @@ print("AUC: ", auc)
 
 # => AUC is 0.66, which is lower than the AUC of the model (only SWAN items as input) (0.79)
 
-### Score SRS manually for ASD
 
-data = all_data[srs_cols + diag_cols]
-data["SRS_score"] = data[srs_cols].sum(axis=1) # https://sci-hub.mksa.top/10.1177/0734282913517525 
+
+### Score SWAN manually for ADHD-Combined Type
+print("Diag.ADHD-Combined Type")
+
+data["Combined_score"] = data[swan_cols].sum(axis=1) # https://www.academia.edu/4479470/Across_the_continuum_of_attention_skills_A_twin_study_of_the_SWAN_ADHD_rating_scale 
+
+# Print mean score
+print(data["Combined_score"].mean())
+# Print mean among non-diagnosed
+print(data[data["Diag.ADHD-Combined Type"] == 0]["Combined_score"].mean())
+# Print mean among diagnosed
+print(data[data["Diag.ADHD-Combined Type"] == 1]["Combined_score"].mean())
+
+# Calculate AUC
+
+auc = roc_auc_score(data["Diag.ADHD-Combined Type"], data["Combined_score"])
+print("AUC: ", auc)
+
+# => AUC is 0.82, which is lower than the AUC of the model (only SWAN items as input) (0.85)
+
+
+
+### Score SRS manually for ASD
+print("Diag.Autism Spectrum Disorder")
+
+data = all_data[srs_cols + diag_cols].copy()
+data["SRS_score"] = data[srs_cols].sum(axis=1) # https://journals.sagepub.com/doi/abs/10.1177/0734282913517525?journalCode=jpaa 
 
 # Print mean score
 print(data["SRS_score"].mean())
