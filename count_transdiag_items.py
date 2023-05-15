63948b21455375976f673db05ad30fc06eab854f
charlie42 <oninfrared@gmail.com>
2023-05-15 15:06:50 +0200

    filter dirs that use all assessments not only free

--- count_transdiag_items.py
+++ count_transdiag_items.py
@@ -7,10 +7,11 @@ import pandas as pd
 def get_newest_non_empty_dir_in_dir(path):
     dir_names = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
     non_empty_dir_names = [d for d in dir_names if len(os.listdir(path+d)) > 0]
-    # Filter only those with first_dropped_assessment in name (not single assessment)
-    non_empty_dir_names = [d for d in non_empty_dir_names if "first_dropped_assessment" in d]
-    # Find non-empty dir with the latest timestamp, dir name format: 2023-01-05 11.03.00___first_dropped_assessment__ICU_P___other_diag_as_input__0___debug_mode__True
     
+    # Filter only those with first_dropped_assessment in name (not single assessment), only_free_assessments == 0
+    non_empty_dir_names = [d for d in non_empty_dir_names if "first_dropped_assessment" in d and not "only_free_assessments__1" in d and "debug_mode__False" in d]
+    
+    # Find non-empty dir with the latest timestamp, dir name format: 2023-01-05 11.03.00___first_dropped_assessment__ICU_P___other_diag_as_input__0___debug_mode__True
     timestamps = [d.split("___")[0] for d in non_empty_dir_names]
     timestamps = [datetime.datetime.strptime(t, "%Y-%m-%d %H.%M.%S") for t in timestamps]
     newest_dir_name = non_empty_dir_names[timestamps.index(max(timestamps))]
@@ -22,6 +23,7 @@ def make_dir_if_not_exists(path):
 
 path = "../diagnosis_predictor_data/reports/identify_feature_subsets/"
 dir = get_newest_non_empty_dir_in_dir(path)
+print("Reading reports from: ", dir)
 
 from joblib import load
 
@@ -63,8 +65,14 @@ item_names["ID"] = item_names["datadic"] + "," + item_names["keys"]
 item_counts_table = item_names.merge(item_counts_table, left_on="ID", right_index=True)
 item_counts_table = item_counts_table.drop(["datadic", "keys"], axis=1)
 
+# Move ID column to be the first
+cols = item_counts_table.columns.tolist()
+cols.pop(cols.index("ID"))
+print(type(["ID"]), type(cols))
+item_counts_table = item_counts_table[["ID"] + cols]
+
 # Sort by count in 126 subset
-item_counts_table = item_counts_table.sort_values(126, ascending=False)
+item_counts_table = item_counts_table.sort_values(30, ascending=False)
 print(item_counts_table)
 
 # Save to csv
