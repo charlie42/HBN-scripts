90add71106cfb2d4f4657a2e685b89a689c1753e
charlie42 <oninfrared@gmail.com>
2023-10-03 10:37:43 +0200

    add multiple test correction to calculate_significance

--- calculate_significance.py
+++ calculate_significance.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import mannwhitneyu
+from statsmodels.stats.multitest import multipletests
 
 from data_reading import DataReader
 
@@ -10,12 +11,22 @@ class SignificanceCalculator:
         self.data = self._remove_none_and_any_diags(data)
 
     def calculate_significance(self, method, features):
+        """"
+        Calculate significance of difference between means of two features
+        """
+
         if method == "mann-whitney":
             statistic, p_value = mannwhitneyu(data[features[0]], self.data[features[1]])
             self._print_significance(p_value)
             return p_value
         else:
             raise ValueError("Method not recognized: ", method)
+        
+    def calculte_multitest(self, method, p_values):
+        """"
+        Correct p-values for multiple testing
+        """
+        return multipletests(p_values, alpha=self.alpha, method=method)[1]
         
     def _print_significance(self, p_value):
         if p_value < self.alpha:
@@ -24,6 +35,9 @@ class SignificanceCalculator:
             print("\nNo significant difference: ", p_value, " >= ", self.alpha)
 
     def _remove_none_and_any_diags(self, data):
+        """
+        Remove "Diag.No Diagnosis Given" and "Diag.Any Diag" from data
+        """
         try:
             result = data.drop("Diag.No Diagnosis Given", axis=0)
         except KeyError:
@@ -42,9 +56,9 @@ if __name__ == "__main__":
 
     # Do tests for ML models 
     print("\nML Models:\n")
+    ml_model_p_values = []
 
     data = data_reader.read_data(data_type="compare_orig_vs_subsets")
-    print(data.columns)
 
     significance_calculator = SignificanceCalculator(data)
     method = "mann-whitney"
@@ -52,28 +66,40 @@ if __name__ == "__main__":
     # Compare means of "AUC all features all assessments parent and sr" and "Best subscale score"
     features = ["AUC all features all assessments parent and sr", "Best subscale score"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    ml_model_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "AUC all features all assessments parent and sr" and "AUC all features all assessments only parent report"
     features = ["AUC all features all assessments parent and sr", "AUC all features all assessments only parent report"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    ml_model_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "AUC all features all assessments parent and sr" and "AUC all features free assessments parent and sr"
     features = ["AUC all features all assessments parent and sr", "AUC all features free assessments parent and sr"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    ml_model_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "AUC all features all assessments parent and sr" and "AUC all features free assessments only parent report"
     features = ["AUC all features all assessments parent and sr", "AUC all features free assessments only parent report"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    ml_model_p_values.append(significance_calculator.calculate_significance(method, features))
+
+    # Correct p-values for multiple testing
+    print("\nCorrecting p-values for multiple testing:\n")
+    print("Before correction: ", ml_model_p_values)
+    ml_model_p_values_corrected = significance_calculator.calculte_multitest(
+        method="bonferroni", 
+        p_values=ml_model_p_values)
+    print("After correction: ", ml_model_p_values_corrected)
+
+
+    ################################################################
+
 
     # Do tests for sum-scores of subsets
     print("\nSum-scores of subsets:\n")
+    sum_scores_p_values = []
 
     data = data_reader.read_data(data_type="sum_score_aurocs")
-    print(data.columns)
 
     significance_calculator = SignificanceCalculator(data)
     method = "mann-whitney"
@@ -81,31 +107,42 @@ if __name__ == "__main__":
     # Compare means of "AUC all features all assessments parent and sr" and "Best subscale score"
     features = ["AUROC all assessments parent and sr", "Best subscale score"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    sum_scores_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "AUC all features all assessments parent and sr" and "AUC all features all assessments only parent report"
     features = ["AUROC all assessments parent and sr", "AUROC all assessments only parent report"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    sum_scores_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "AUC all features all assessments parent and sr" and "AUC all features free assessments parent and sr"
     features = ["AUROC all assessments parent and sr", "AUROC free assessments parent and sr"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    sum_scores_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "AUC all features all assessments parent and sr" and "AUC all features free assessments only parent report"
     features = ["AUROC all assessments parent and sr", "AUROC free assessments only parent report"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    sum_scores_p_values.append(significance_calculator.calculate_significance(method, features))
+
+    # Correct p-values for multiple testing
+    print("\nCorrecting p-values for multiple testing:\n")
+    print("Before correction: ", sum_scores_p_values)
+    sum_scores_p_values_corrected = significance_calculator.calculte_multitest(
+        method="bonferroni", 
+        p_values=sum_scores_p_values)
+    print("After correction: ", sum_scores_p_values_corrected)
+
+
+    ################################################################
+
 
     # Do tests for learning improvements
     print("\nLearning improvements:\n")
+    learning_improvements_p_values = []
 
     data = data_reader.read_data(data_type="learning_improvements")
-    print(data.columns)
 
     ## Only learning diags
-    print("DEBUG", data.index)
     learning_diags = [
         'Diag.Specific Learning Disorder with Impairment in Reading (test)',
         'Diag.NVLD without reading condition (test)',
@@ -123,16 +160,24 @@ if __name__ == "__main__":
     # Compare means of "original" and "more assessments"
     features = ["original", "more assessments"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    learning_improvements_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "original" and "more assessments and NIH"
     features = ["original", "more assessments and NIH"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    learning_improvements_p_values.append(significance_calculator.calculate_significance(method, features))
 
     # Compare means of "more assessments " and "more assessments and NIH"
     features = ["more assessments", "more assessments and NIH"]
     print(f"Comparing: {features}:")
-    significance_calculator.calculate_significance(method, features)
+    learning_improvements_p_values.append(significance_calculator.calculate_significance(method, features))
+
+    # Correct p-values for multiple testing
+    print("\nCorrecting p-values for multiple testing:\n")
+    print("Before correction: ", learning_improvements_p_values)
+    learning_improvements_p_values_corrected = significance_calculator.calculte_multitest(
+        method="bonferroni", 
+        p_values=learning_improvements_p_values)
+    print("After correction: ", learning_improvements_p_values_corrected)
 
 
